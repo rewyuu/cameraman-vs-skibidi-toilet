794570e11dd560e27939497ebb98703c54d5a14d
rewyuu <reybenedictrico123@gmail.com>
2024-06-08 10:13:21 +0800

    final na tlaga

--- register-login/database.sql
+++ register-login/database.sql
@@ -3,13 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2024 at 03:48 AM
+-- Generation Time: Jun 08, 2024 at 04:12 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+00:00"
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
